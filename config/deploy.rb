4c26103295000e99e02820b5dd2be39884a496f6
Ben Nolan <bnolan@gmail.com>
2015-09-18 12:08:53 +1200

    Add capistrano deploy recipe.

---
+++ config/deploy.rb
@@ -0,0 +1,64 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'scene-www'
+set :repo_url, 'git@github.com:scenevr/www.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/ben/www'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  before :starting, :run_tests
+
+  task :run_tests do
+    unless system "npm test"
+      puts "Tests failed."
+      exit
+    end
+  end
+
+  before :starting, :stop_forever
+
+  task :stop_forever do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "forever stop /home/ben/www/current/server.js; true"
+    end
+  end
+
+  before :published, :start_forever
+
+  task :start_forever do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "cd #{release_path}; npm install"
+      execute "NODE_ENV=production forever start /home/ben/www/current/server.js"
+    end
+  end
+end
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Deploying
+
+## scenevr-client
+
+npm version patch
+npm publish
+
+## scenevr-www
+
+npm install --save scene-client
+git commit -a -m "bump scene-client"
+git push
+cap production deploy
